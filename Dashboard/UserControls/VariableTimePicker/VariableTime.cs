c1ddbdf6363336020858c9f638983e556decf474
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-11-24 14:47:14 +0530

    variabletime poc done

---
+++ Dashboard/UserControls/VariableTimePicker/VariableTime.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.UserControls.VariableTimePicker
+{
+    public class VariableTime
+    {
+        public bool IsYearsVariable { get; set; } = false;
+        public bool IsMonthsVariable { get; set; } = false;
+        public bool IsDaysVariable { get; set; } = false;
+        public bool IsHoursVariable { get; set; } = false;
+        public bool IsMinutesVariable { get; set; } = false;
+        public bool IsSecondsVariable { get; set; } = false;
+        public int YearsOffset { get; set; } = 0;
+        public int MonthsOffset { get; set; } = 0;
+        public int DaysOffset { get; set; } = 0;
+        public int HoursOffset { get; set; } = 0;
+        public int MinutesOffset { get; set; } = 0;
+        public int SecondsOffset { get; set; } = 0;
+
+        public DateTime AbsoluteTime { get; set; } = DateTime.Now;
+
+        internal VariableTime Clone()
+        {
+            VariableTime variableTime = new VariableTime
+            {
+                IsYearsVariable = IsYearsVariable,
+                IsMonthsVariable = IsMonthsVariable,
+                IsDaysVariable = IsDaysVariable,
+                IsHoursVariable = IsHoursVariable,
+                IsMinutesVariable = IsMinutesVariable,
+                IsSecondsVariable = IsSecondsVariable,
+                YearsOffset = YearsOffset,
+                MonthsOffset = MonthsOffset,
+                DaysOffset = DaysOffset,
+                HoursOffset = HoursOffset,
+                MinutesOffset = MinutesOffset,
+                SecondsOffset = SecondsOffset,
+                AbsoluteTime = AbsoluteTime
+            };
+            return variableTime;
+        }
+
+        public DateTime GetTime()
+        {
+            DateTime time = AbsoluteTime;
+            if (IsYearsVariable) { time.AddYears(YearsOffset); }
+            if (IsMonthsVariable) { time.AddMonths(MonthsOffset); }
+            if (IsDaysVariable) { time.AddDays(DaysOffset); }
+            if (IsHoursVariable) { time.AddHours(HoursOffset); }
+            if (IsMinutesVariable) { time.AddHours(MinutesOffset); }
+            if (IsSecondsVariable) { time.AddSeconds(SecondsOffset); }
+            return time;
+        }
+    }
+}
---
+++ Dashboard/UserControls/VariableTimePicker/VariableTimePicker.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard.UserControls.VariableTimePicker
+{
+    /// <summary>
+    /// Interaction logic for VariableTimePicker.xaml
+    /// </summary>
+    public partial class VariableTimePicker : UserControl
+    {
+        public VariableTimePickerVM PickerVM { get; set; }
+        public VariableTimePicker()
+        {
+            InitializeComponent();
+            PickerVM = new VariableTimePickerVM(new VariableTime());
+            DataContext = PickerVM;
+        }
+    }
+    public class VariableTimePickerVM
+    {
+        public VariableTime _VariableTime { get; set; }
+        public VariableTimePickerVM(VariableTime variableTime)
+        {
+            _VariableTime = variableTime.Clone();
+        }
+    }
+}
