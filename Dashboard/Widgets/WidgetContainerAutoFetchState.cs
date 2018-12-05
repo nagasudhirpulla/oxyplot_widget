52ba0b282d740c978eb2af1d43061d530a32c431
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-12-05 10:11:50 +0530

    widget frame auto fetch started

---
+++ Dashboard/Widgets/WidgetContainerAutoFetchState.cs
@@ -0,0 +1,29 @@
+﻿using Dashboard.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Widgets
+{
+    public class WidgetContainerAutoFetchState
+    {
+        public SchedulerState SchedulerState { get; set; } = new SchedulerState();
+
+        public TimeSpan GetTimerPeriod()
+        {
+            TimeSpan span = SchedulerState.FetchPeriodicity;
+            return span;
+        }
+
+        public WidgetContainerAutoFetchState Clone()
+        {
+            WidgetContainerAutoFetchState state = new WidgetContainerAutoFetchState
+            {
+                SchedulerState = SchedulerState.Clone()
+            };
+            return state;
+        }
+    }
+}
