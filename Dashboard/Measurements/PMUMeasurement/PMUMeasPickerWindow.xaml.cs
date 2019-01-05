980b7060d724d2cadaef283fc9c17cc0211d5797
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-01-05 23:12:26 +0530

    lazy loading done

--- Dashboard/Measurements/PMUMeasurement/PMUMeasPickerWindow.xaml.cs
+++ Dashboard/Measurements/PMUMeasurement/PMUMeasPickerWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Dashboard.Measurements.PMUMeasurement
     public partial class PMUMeasPickerWindow : Window
     {
         // https://stackoverflow.com/questions/12052565/binding-xdocument-to-treeview-attributes-dont-show-up
+        // lazy loading - https://www.wpf-tutorial.com/treeview-control/lazy-loading-treeview-items/
         public PMUMeasPickerWindow()
         {
             InitializeComponent();
@@ -38,13 +39,56 @@ namespace Dashboard.Measurements.PMUMeasurement
             HistoryDataAdapter adapter = new HistoryDataAdapter(configManager);
             measXml = adapter.GetMeasXml();
             // Bind the tree view with xml
-            MeasTree.DataContext = measXml;
             SetTreeViewElements(measXml);
         }
 
         private void SetTreeViewElements(XDocument measXml)
         {
-            MeasTree.DataContext = measXml;
+            MeasTree.Items.Add(CreateTreeItem(measXml.Root));
+        }
+
+        private TreeViewItem CreateTreeItem(object o)
+        {
+            TreeViewItem item = new TreeViewItem();
+            item.Tag = o;
+            if (o is XElement xEl)
+            {
+                item.Header = xEl.Name.LocalName;
+            }
+            else
+            {
+                item.Header = o.ToString();
+            }
+            item.Items.Add("Loading...");
+            return item;
+        }
+
+        public void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.Source as TreeViewItem;
+            if ((item.Items.Count == 1) && (item.Items[0] is string))
+            {
+                item.Items.Clear();
+                try
+                {
+                    if (item.Tag is XElement xEl)
+                    {
+                        // if tag is has elements then create tree item
+                        if (xEl.HasElements == true)
+                        {
+                            foreach (var xElItem in xEl.Elements())
+                            {
+                                item.Items.Add(CreateTreeItem(xElItem));
+                            }
+                        }
+                        else
+                        {
+                            item.Items.Add(new TreeViewItem { Header = xEl.Value.ToString() });
+                        }
+                    }
+                }
+                catch { }
+            }
         }
 
         private void RefreshBtn_Click(object sender, RoutedEventArgs e)
@@ -67,6 +111,6 @@ namespace Dashboard.Measurements.PMUMeasurement
         {
             string testXml = "<guestbook><guest><fname>Terje</fname><lname>Beck</lname></guest><guest><fname>Jan</fname><lname>Refsnes</lname></guest><guest><fname>Torleif</fname><lname>Rasmussen</lname></guest><guest><fname>anton</fname><lname>chek</lname></guest><guest><fname>stale</fname><lname>refsnes</lname></guest><guest><fname>hari</fname><lname>prawin</lname></guest><guest><fname>Hege</fname><lname>Refsnes</lname></guest></guestbook>";
             SetTreeViewElements(XDocument.Parse(testXml));
-        }        
+        }
     }
 }
